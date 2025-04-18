ba4c446c2f6d3d4372b16061bb4bc7cd2e1aebce
zak1sn0good <zaki97.usma@gmail.com>
2025-04-18 04:30:51 +0100

    add svg configuration, install axios

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svg-react" />
+
+declare module "*.svg?react" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default ReactComponent;
+}
+
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
